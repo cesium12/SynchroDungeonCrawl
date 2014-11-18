40345c16513294a083537bb23e5e8e61276177ba
Mark Mascaro <doublemark@gmail.com>
2014-11-18 12:20:25 -0500

    Add a skeletal Resource.
    We really need some specification here, whoops.

--- SynchroDungeonCrawl/server/data.js
+++ SynchroDungeonCrawl/server/data.js
@@ -18,7 +18,6 @@ exports.Coordinates = Coordinates;
  * allowing you to do order-independent calculations. You can get()
  * the final value.
  */
-
 function Value(add, mul) {
   this.add = (typeof add === "undefined") ? 0 : add;
   this.mul = (typeof mul === "undefined") ? 1 : mul;
@@ -44,3 +43,39 @@ Value.foldValues = function(values) {
 }
 
 exports.Value = Value;
+
+/**
+ * Resource: A container for a unit's resource. Contains a value
+ * and display information, and possibly a max.
+ * amount: integer, the current value of the resource
+ * displayInfo: TODO(cesium)
+ * params: supports any of the following:
+ *   max: number, resource maximum value, default no limit
+ *   min: number, resource minimum value, default zero, null means no limit
+ */
+function Resource(amount, displayInfo, params) {
+  this.amount = amount;
+  this.displayInfo = displayInfo;
+  this.params = (typeof params === "undefined") ? {} : params;
+  if (typeof this.params.min === "undefined") {
+    this.params.min = 0;
+  }
+}
+
+/**
+ * Spends (positive input) or recovers (negative input) a resource,
+ * clipping the result to acceptable values.
+ * amount: number, the amount by which to reduce the resource
+ */
+Resource.prototype.spend(amount) {
+  this.amount -= amount;
+  if (this.params.max && this.amount > this.params.max) {
+    this.amount = this.params.max;
+  } else if (this.params.min && this.amount < this.params.min) {
+    this.amount = this.params.min;
+  }
+}
+
+// DISPLAY INFO FUNCTIONALITY GOES HERE
+
+export.Resource = Resource;
